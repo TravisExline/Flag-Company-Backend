fa505ff099d9d31173f20fc26452d89ed5ac821c
TravisExline <t.exline22@gmail.com>
2020-08-05 08:09:18 -0500

    seeded flag data

--- flag-company-backend/db/seeds.rb
+++ flag-company-backend/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+flags = Flag.create([{name: 'Black American Flag', smallPrice: 55, medPrice: 75, lrgPrice: 110},
+{name: 'American Flag', smallPrice: 55, medPrice: 75, lrgPrice: 110},
+{name: 'Thin Blue Line American Flag', smallPrice: 55, medPrice: 75, lrgPrice: 110},
+{name: 'Punisher American Flag', smallPrice: 55, medPrice: 75, lrgPrice: 110},
+{name: 'Tattered American Flag', smallPrice: 55, medPrice: 75, lrgPrice: 110},
+{name: 'USAF American Flag', smallPrice: 55, medPrice: 75, lrgPrice: 0},
+{name: 'Tread American Flag', smallPrice: 55, medPrice: 75, lrgPrice: 110},
+{name: 'US Navy American Flag', smallPrice: 55, medPrice: 75, lrgPrice: 0},
+{name: 'Thin Red Line American Flag', smallPrice: 55, medPrice: 75, lrgPrice: 110}])
